78f2cef2269ed01698a2053a96ed970dd867b329
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-01 16:00:31 +0100

    Sixth Commit: Implement AdminLogin Class to handle admin user login.

---
+++ src/java/servlets/AdminLogin.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+/**
+ *
+ * @author adrianadewunmi
+ */
+@WebServlet("/servlets.AdminLogin")
+public class AdminLogin extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.println("<head>");
+		out.println("<title>Admin Section</title>");
+		out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+                
+                String email=request.getParameter("email");
+		String password=request.getParameter("password");
+                //Default password & email
+                String e1 = "admin@unv.com";
+                String p1 ="admin123";
+		if(email.equals(e1)&&password.equals(p1)){
+			HttpSession session=request.getSession();
+			session.setAttribute("admin","true");
+			
+			request.getRequestDispatcher("navadmin.html").include(request, response);
+			request.getRequestDispatcher("admincarousel.html").include(request, response);
+			
+		}else{
+			request.getRequestDispatcher("navhome.html").include(request, response);
+			out.println("<div class='container'>");
+			out.println("<h3>Username or password error</h3>");
+			request.getRequestDispatcher("adminloginform.html").include(request, response);
+			out.println("</div>");
+		}
+		
+		
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
+    }
+
+
+}
