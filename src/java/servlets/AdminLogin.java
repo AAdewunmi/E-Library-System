3a846840b1f7f3e8cca3a4246550d01d92ebf697
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-11 14:26:11 +0100

    Twenty-Eighth Commit: Add Bootstrap starter template to addlibrarianform.html

--- src/java/servlets/AdminLogin.java
+++ src/java/servlets/AdminLogin.java
@@ -61,11 +61,11 @@ public class AdminLogin extends HttpServlet {
 			request.getRequestDispatcher("navhome.html").include(request, response);
 			out.println("<div class='container'>");
 			out.println("<h3>Username or password error!</h3>");
-			request.getRequestDispatcher("adminloginform.html").include(request, response);
+                        request.getRequestDispatcher("adminloginform.html").include(request, response);
 			out.println("</div>");
+                        
+                       
 		}
-		
-		
 		request.getRequestDispatcher("footer.html").include(request, response);
 		out.close();
     }
