5f505aadaa707a60fccf34063fd80d788666b495
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-15 15:49:13 +0100

    Thirty-Five Commit:  Change Database name from "elibrary_DB" to "ELibraryDB" / Create ELibraryDB MySQL Database.

--- src/java/dao/DB.java
+++ src/java/dao/DB.java
@@ -4,10 +4,20 @@
  */
 package dao;
 
+import java.sql.Connection;
+
 /**
  *
  * @author adrianadewunmi
  */
 public class DB {
+    public static Connection getCon(){
+        
+        Connection connection = null;
+        String userName = "root";
+        String userPassword = "abc";
+        
+        Class.forName("");
     
+    }
 }
