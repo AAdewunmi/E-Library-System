3a99640e7e5fce809d15791d3a3e642008145f72
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-15 16:38:58 +0100

    Thirty-Seven Commit:  Implement LibrarianBean class.

--- src/java/beans/LibrarianBean.java
+++ src/java/beans/LibrarianBean.java
@@ -10,4 +10,63 @@ package beans;
  */
 public class LibrarianBean {
     
+    private int librarianID;
+    private String librarianName, librarianEmail, librarianPassword;
+    private long librarianMobileNumber;
+
+    public LibrarianBean(int librarianID, String librarianName, String librarianEmail, String librarianPassword, long librarianMobileNumber) {
+        this.librarianID = librarianID;
+        this.librarianName = librarianName;
+        this.librarianEmail = librarianEmail;
+        this.librarianPassword = librarianPassword;
+        this.librarianMobileNumber = librarianMobileNumber;
+    }
+
+    public LibrarianBean(String librarianName, String librarianEmail, String librarianPassword, long librarianMobileNumber) {
+        this.librarianName = librarianName;
+        this.librarianEmail = librarianEmail;
+        this.librarianPassword = librarianPassword;
+        this.librarianMobileNumber = librarianMobileNumber;
+    }
+
+    public int getLibrarianID() {
+        return librarianID;
+    }
+
+    public void setLibrarianID(int librarianID) {
+        this.librarianID = librarianID;
+    }
+
+    public String getLibrarianName() {
+        return librarianName;
+    }
+
+    public void setLibrarianName(String librarianName) {
+        this.librarianName = librarianName;
+    }
+
+    public String getLibrarianEmail() {
+        return librarianEmail;
+    }
+
+    public void setLibrarianEmail(String librarianEmail) {
+        this.librarianEmail = librarianEmail;
+    }
+
+    public String getLibrarianPassword() {
+        return librarianPassword;
+    }
+
+    public void setLibrarianPassword(String librarianPassword) {
+        this.librarianPassword = librarianPassword;
+    }
+
+    public long getLibrarianMobileNumber() {
+        return librarianMobileNumber;
+    }
+
+    public void setLibrarianMobileNumber(long librarianMobileNumber) {
+        this.librarianMobileNumber = librarianMobileNumber;
+    }
+    
 }
