4e8a615f408745a27323782bea4059a97ae08990
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-16 12:38:00 +0100

    Forty-One Commit: Implement "view" method in LibrarianDao class

--- src/java/dao/LibrarianDao.java
+++ src/java/dao/LibrarianDao.java
@@ -7,6 +7,9 @@ package dao;
 import beans.LibrarianBean;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -16,7 +19,6 @@ public class LibrarianDao {
     
     public static int save(LibrarianBean bean){
 		int status=0;
-                
 		try{
 			Connection con=DB.getCon();
 			PreparedStatement ps=con.prepareStatement("insert into e_librarian(id,name,email,password,mobile) values(?,?,?,?,?)");
@@ -51,6 +53,26 @@ public class LibrarianDao {
 		return status;
     }
     
-    
+    public static List<LibrarianBean> view(){
+		List<LibrarianBean> list=new ArrayList<LibrarianBean>();
+		try{
+			Connection con=DB.getCon();
+			PreparedStatement ps=con.prepareStatement("select * from e_librarian");
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()){
+				LibrarianBean bean=new LibrarianBean();
+				bean.setLibrarianID(rs.getInt("id"));
+				bean.setLibrarianName(rs.getString("name"));
+				bean.setLibrarianEmail(rs.getString("email"));
+				bean.setLibrarianPassword(rs.getString("password"));
+				bean.setLibrarianMobileNumber(rs.getLong("mobile"));
+				list.add(bean);
+			}
+			con.close();
+			
+		}catch(Exception e){System.out.println(e);}
+		
+		return list;
+	}
     
 }
