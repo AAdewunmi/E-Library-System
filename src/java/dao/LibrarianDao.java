364234e77e7a1e296e3696b7000b8dce9e3e8a14
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-16 12:21:41 +0100

    Forty Commit: Implement "save" method in LibrarianDao class

--- src/java/dao/LibrarianDao.java
+++ src/java/dao/LibrarianDao.java
@@ -4,10 +4,33 @@
  */
 package dao;
 
+import beans.LibrarianBean;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author adrianadewunmi
  */
 public class LibrarianDao {
+    
+    public static int save(LibrarianBean bean){
+		int status=0;
+                
+		try{
+			Connection con=DB.getCon();
+			PreparedStatement ps=con.prepareStatement("insert into e_librarian(id,name,email,password,mobile) values(?,?,?,?,?)");
+			ps.setString(1,"1");
+                        ps.setString(2,bean.getLibrarianName());
+			ps.setString(3,bean.getLibrarianEmail());
+			ps.setString(4,bean.getLibrarianPassword());
+			ps.setLong(5,bean.getLibrarianMobileNumber());
+			status=ps.executeUpdate();
+			con.close();
+			
+		}catch(Exception e){System.out.println(e);}
+		
+		return status;
+	}
     
 }
