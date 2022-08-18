7724c57421511fb1ab8556e38415e424e3e3bdb4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-18 16:45:34 +0100

    Fifty-Four Commit: Implement AddBookForm Class, provides controller functionality for "Add Book Form.

--- src/java/servlets/AddBookForm.java
+++ src/java/servlets/AddBookForm.java
@@ -6,42 +6,18 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
 
 /**
  *
  * @author adrianadewunmi
  */
-public class AddBookForm extends HttpServlet {
+import dao.*;
+@WebServlet("/servlets.AddBookForm")
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AddBookForm</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AddBookForm at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+public class AddBookForm extends HttpServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -55,31 +31,25 @@ public class AddBookForm extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        out.print("<!DOCTYPE html>");
+        out.print("<html>");
+        out.println("<head>");
+        out.println("<title>Add Book Form</title>");
+        out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+        out.println("</head>");
+        out.println("<body>");
+        request.getRequestDispatcher("navlibrarian.html").include(request, response);
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+        out.println("<div class='container'>");
+        request.getRequestDispatcher("addbookform.html").include(request, response);
+        out.println("</div>");
+
+
+        request.getRequestDispatcher("footer.html").include(request, response);
+        out.close();
+    }
 
 }
