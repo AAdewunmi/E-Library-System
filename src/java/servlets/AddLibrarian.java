2449449932f9b72567d04f120c302639b76ec30b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-20 12:33:07 +0100

    Sixty-Two Commit: Implement AddLibrarian Class which functions as a controller for adding new librarian.

--- src/java/servlets/AddLibrarian.java
+++ src/java/servlets/AddLibrarian.java
@@ -6,57 +6,20 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+import dao.*;
+import beans.*;
 
 /**
  *
  * @author adrianadewunmi
  */
+
+@WebServlet("/servlets.AddLibrarian")
 public class AddLibrarian extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AddLibrarian</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AddLibrarian at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -69,17 +32,37 @@ public class AddLibrarian extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+        out.print("<!DOCTYPE html>");
+        out.print("<html>");
+        out.println("<head>");
+        out.println("<title>Add Librarian</title>");
+        out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+        out.println("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css/'>");
+        out.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js/'></script>");
+        out.println("<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js/'></script>");
+        out.println("</head>");
+        out.println("<body>");
+        
+        request.getRequestDispatcher("navadmin.html").include(request, response);
+	out.println("<div class='container'>");
+        
+        String name=request.getParameter("name");
+        String email=request.getParameter("email");
+        String password=request.getParameter("password");
+        String smobile=request.getParameter("mobile");
+        long mobile=Long.parseLong(smobile);
+        LibrarianBean bean=new LibrarianBean(name, email, password, mobile);
+        LibrarianDao.save(bean);
+        out.print("<h4>Librarian added successfully</h4>");
+        request.getRequestDispatcher("addlibrarianform.html").include(request, response);
+
+
+        out.println("</div>");
+        request.getRequestDispatcher("footer.html").include(request, response);
+        out.close();
+    }
 
 }
