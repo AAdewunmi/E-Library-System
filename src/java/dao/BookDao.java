ec44162ddcde0c742ed2e2b4cba14e0e5a0cb35f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-22 14:58:00 +0100

    Sixty-Eight Commit: Implement save method in BookDao Class

--- src/java/dao/BookDao.java
+++ src/java/dao/BookDao.java
@@ -8,6 +8,33 @@ package dao;
  *
  * @author adrianadewunmi
  */
+
+import beans.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BookDao {
+    
+    public static int save(BookBean bean){
+        int status = 0;
+        
+        try{
+            Connection con = DB.getCon();
+            PreparedStatement ps = con.prepareStatement("insert into e_book values(?,?,?,?,?,?)");
+            ps.setString(1, bean.getCallno());
+            ps.setString(2, bean.getName());
+            ps.setString(3, bean.getAuthor());
+            ps.setString(4, bean.getPublisher());
+            ps.setInt(5, bean.getQuantity());
+            ps.setInt(6, 0);
+            status = ps.executeUpdate();
+            con.close();
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println("SAVE ERROR! " + e);
+        }
+        
+        return status;
+    }
     
 }
