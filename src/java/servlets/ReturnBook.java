a07a2253f8d00ee9b3ada4c4198dcede22b0e2f0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-27 16:46:36 +0100

    Eighty-Nine Commit: Implement ReturnBook Class which handles controller functionality for users to return books

--- src/java/servlets/ReturnBook.java
+++ src/java/servlets/ReturnBook.java
@@ -6,57 +6,17 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+import beans.IssueBookBean;
+import dao.BookDao;
 /**
  *
  * @author adrianadewunmi
  */
+@WebServlet("/servlets.ReturnBook")
 public class ReturnBook extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ReturnBook</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ReturnBook at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -69,17 +29,38 @@ public class ReturnBook extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+
+        out.print("<!DOCTYPE html>");
+        out.print("<html>");
+        out.println("<head>");
+        out.println("<title>Return Book</title>");
+        out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+        out.println("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css/'>");
+        out.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js/'></script>");
+        out.println("<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js/'></script>");
+        out.println("</head>");
+        out.println("<body>");
+        request.getRequestDispatcher("navlibrarian.html").include(request, response);
+        
+        out.println("<div class='container'>");
+        String callno=request.getParameter("callno");
+        String sstudentid=request.getParameter("studentid");
+        int studentid=Integer.parseInt(sstudentid);
+
+        int i=BookDao.returnBook(callno,studentid);
+        if(i>0){
+                out.println("<h3>Book returned successfully</h3>");
+        }else{
+                out.println("<h3>Sorry, unable to return book.</h3><p>We may have sortage of books. Kindly visit later.</p>");
+        }
+        out.println("</div>");
+
+
+        request.getRequestDispatcher("footer.html").include(request, response);
+        out.close();
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
