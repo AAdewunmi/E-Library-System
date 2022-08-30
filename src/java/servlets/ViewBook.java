0547c5c60126d4bd6343c4afb96ddea1d02db106
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-30 09:46:32 +0100

    Hundred Commit: Implement ViewBook Class which handles function for viewing all books.

--- src/java/servlets/ViewBook.java
+++ src/java/servlets/ViewBook.java
@@ -6,13 +6,17 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
 
 /**
  *
  * @author adrianadewunmi
  */
-
+import beans.*;
+import dao.*;
+import java.util.List;
 
 @WebServlet("/servlets.ViewBook")
 
@@ -31,6 +35,36 @@ public class ViewBook extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        out.print("<!DOCTYPE html>");
+        out.print("<html>");
+        out.println("<head>");
+        out.println("<title>View Book</title>");
+        out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+        out.println("</head>");
+        out.println("<body>");
+       
+        request.getRequestDispatcher("navlibrarian.html").include(request, response);
+        
+        out.println("<div class='container'>");
+        
+        List<BookBean> list = BookDao.view();
+        
+        out.println("<table class='table table-bordered table-striped'>");
+	out.println("<tr><th>Callno</th><th>Name</th><th>Author</th><th>Publisher</th><th>Quantity</th><th>Issued</th><th>Delete</th></tr>");
+        
+        for(BookBean bean:list){
+            out.println("<tr><td>"+bean.getCallno()+"</td><td>"+bean.getName()+"</td><td>"+bean.getAuthor()+"</td><td>"+bean.getPublisher()+"</td><td>"+bean.getQuantity()+"</td><td>"+bean.getIssued()+"</td><td><a href='DeleteBook?callno="+bean.getCallno()+"'>Delete</a></td></tr>");
+	}
+        
+        out.println("</table>");
+		
+        out.println("</div>");
+
+        request.getRequestDispatcher("footer.html").include(request, response);
+        out.close();
     }
 
 }
