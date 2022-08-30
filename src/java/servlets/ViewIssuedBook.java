8a805f43946677f4582631240ac9c1b85632b62b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-30 14:17:29 +0100

    One Hundred-Six Commit: Implement ViewIssuedBook Class which handles functionality for viewing issued books.

--- src/java/servlets/ViewIssuedBook.java
+++ src/java/servlets/ViewIssuedBook.java
@@ -6,10 +6,16 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+import beans.BookBean;
+import beans.IssueBookBean;
+import dao.BookDao;
+import java.util.List;
+
+@WebServlet("/servlets.ViewIssuedBook")
 
 /**
  *
@@ -17,31 +23,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ViewIssuedBook extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ViewIssuedBook</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ViewIssuedBook at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -55,31 +36,35 @@ public class ViewIssuedBook extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        out.print("<!DOCTYPE html>");
+        out.print("<html>");
+        out.println("<head>");
+        out.println("<title>View Issued Book</title>");
+        out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+        out.println("</head>");
+        out.println("<body>");
+        request.getRequestDispatcher("navlibrarian.html").include(request, response);
+        
+        out.println("<div class='container'>");
+		
+        List<IssueBookBean> list=BookDao.viewIssuedBook();
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        out.println("<table class='table table-bordered table-striped'>");
+        out.println("<tr><th>Callno</th><th>Student Id</th><th>Student Name</th><th>Student Mobile</th><th>Issued Date</th><th>Return Status</th></tr>");
+        for(IssueBookBean bean:list){
+                out.println("<tr><td>"+bean.getCallno()+"</td><td>"+bean.getStudentid()+"</td><td>"+bean.getStudentname()+"</td><td>"+bean.getStudentmobile()+"</td><td>"+bean.getIssuedate()+"</td><td>"+bean.getReturnstatus()+"</td></tr>");
+        }
+        out.println("</table>");
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+        out.println("</div>");
+
+
+        request.getRequestDispatcher("footer.html").include(request, response);
+        out.close();
+        
+    }
 
 }
