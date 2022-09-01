1b6606a685b66d30fe6a567db541f7331a7ba9b6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-09-01 12:33:11 +0100

    One Hundred-Fifteen Commit: Implement controller for librarian login.

--- src/java/servlets/LibrarianLogin.java
+++ src/java/servlets/LibrarianLogin.java
@@ -6,57 +6,18 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
 
 /**
  *
  * @author adrianadewunmi
  */
+import dao.LibrarianDao;
+@WebServlet("/servlets.LibrarianLogin")
+
 public class LibrarianLogin extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LibrarianLogin</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LibrarianLogin at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -69,17 +30,36 @@ public class LibrarianLogin extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        out.print("<!DOCTYPE html>");
+        out.print("<html>");
+        out.println("<head>");
+        out.println("<title>Librarian Section</title>");
+        out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+        out.println("</head>");
+        out.println("<body>");
+        
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        if(LibrarianDao.authenticate(email, password)){
+            HttpSession session = request.getSession();
+            session.setAttribute("email", email);
+            request.getRequestDispatcher("navlibrarian.html").include(request, response);
+            request.getRequestDispatcher("librariancarousel.html").include(request, response);
+        }else{
+            request.getRequestDispatcher("navhome.html").include(request, response);
+            out.println("<div class='container'>");
+            out.println("<h3>Username or password error</h3>");
+            request.getRequestDispatcher("librarianloginform.html").include(request, response);
+            out.println("</div>");
+        }
+        
+        request.getRequestDispatcher("footer.html").include(request, response);
+	out.close();
+       
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
